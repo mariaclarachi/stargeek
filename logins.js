0c6e54159a4de181b2545229183683f9e5cfd838
maria clara <maria.basiqueto@portalsesisp.org.br>
2023-05-16 14:50:45 -0300

    logica 16052023

---
+++ logins.js
@@ -0,0 +1,17 @@
+const mensagem = document.querySelector("mensagem");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const formulario = document.getElementById("formulario");
+
+formulario.onsubmit = (evento) => {
+    let dados = JSON.parse(localStorage.getItem("dados"));
+
+    dados.array.forEach((elemento) => {
+        if (elemento.email == email.value && elemento.senha == senha.value) {
+            evento.preventDefault();
+            return true;
+        } else {
+            mensagem.innerHTML ="Senha ou E-mail Incorreto"
+        }
+    });
+}
